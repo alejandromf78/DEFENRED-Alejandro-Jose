d6f1bdcd0fe082065a52fe1856bfed1d3c03c924
alejandromf78 <151619408+alejandromf78@users.noreply.github.com>
2025-01-28 09:19:37 +0100

    tamano logo

--- DEFENRED/defenred1/defenred1/src/components/header.tsx
+++ DEFENRED/defenred1/defenred1/src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="grid grid20-80">
       <div className="Logo">
-        <img src="../medios/Logos/Logo.png" alt="logo" />
+        <img className="imagenLogo" src="../medios/Logos/Logo.png" alt="logo"  />
       </div>
       <nav className="menu grid grid33">
         <a href="">Inicio</a>
