efc6be6d747094c60840ebed0000144e1cef9f49
joseaandres12 <160121556+joseaandres12@users.noreply.github.com>
2025-02-04 14:00:30 +0100

    55

--- DEFENRED/defenred1/defenred1/src/layout.tsx
+++ DEFENRED/defenred1/defenred1/src/layout.tsx
@@ -1,10 +1,12 @@
 import{Outlet} from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
+import Banner from './components/Banner';
 
 const Layout=()=>{
     return(
         <>
+        <Banner></Banner>
         <Header></Header>
         <Outlet></Outlet>
         <Footer></Footer>
