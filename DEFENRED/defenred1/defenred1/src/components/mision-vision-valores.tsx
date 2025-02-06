997c2217024253694c5233794bc7ab45df2d24b3
joseaandres12 <160121556+joseaandres12@users.noreply.github.com>
2025-02-06 14:03:14 +0100

    dk

--- DEFENRED/defenred1/defenred1/src/components/mision-vision-valores.tsx
+++ DEFENRED/defenred1/defenred1/src/components/mision-vision-valores.tsx
@@ -2,7 +2,7 @@ import "../components/css/mision-vision-valores.css";
 
 const MisionVisionValores = () => {
     return (
-        <div className='inicio mision'>
+        <div className='mision'>
             <div className="div-mision">
                 <div className="contenido">
 
@@ -13,12 +13,35 @@ const MisionVisionValores = () => {
 
                     </div>
 
-                    <div className="grid-33">
+                    <div className="grid-20">
+                        <div></div>
                         <div className="mision-defenred">
+                            <img src="../public/medios/Fotografías e ilustraciones/bosque rojizo.png" alt="" className="imagen-bosque" />
                             <h3>Misión</h3>
+                            <p>Nuestra misión es apoyar a los defensores de derechos
+                                humanos en un entorno seguro y de autocuidado, impulsando el bienestar y la
+                                justicia social desde la Casa de Respiro. Descubre nuestros valores y visión de
+                                un mundo más justo y equitativo.</p>
                         </div>
-                        <div className="vision-defenred">Vision</div>
-                        <div className="valores-defenred">Valores</div>
+                        <div className="vision-defenred">
+                            <img src="../public/medios/Fotografías e ilustraciones/bosque rojizo.png" alt="" className="imagen-bosque" />
+                            <h3>Visión</h3>
+                            <p>Promover el <strong> bienestar integral </strong> de las <strong> personas defensoras </strong> de los
+                                <strong> derechos humanos </strong> a través del programa <strong> "Casa de Respiro" </strong> , proporcionando un
+                                espacio seguro para la reflexión y el <strong> autocuidado </strong>, con el fin de fortalecer su
+                                capacidad para continuar su labor de defensa en un entorno más saludable y
+                                sostenible.</p>
+                        </div>
+                        <div className="valores-defenred">
+                            <img src="../public/medios/Fotografías e ilustraciones/bosque rojizo.png" alt="" className="imagen-bosque" />
+                            <h3>Valores</h3>
+                            <p>Crear un mundo en el que todas las personas defensoras de los derechos
+                                humanos tengan acceso a recursos de autocuidado y apoyo emocional, lo que les
+                                permita vivir plenamente y ejercer su labor sin comprometer su bienestar personal,
+                                contribuyendo así a la mejora de sus comunidades y a la promoción de la justicia
+                                social.</p>
+                        </div>
+                        <div></div>
                     </div>
 
 
