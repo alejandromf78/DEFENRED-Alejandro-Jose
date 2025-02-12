8ae204af05420826eec3c493f23052b5daff32f0
alejandromf78 <151619408+alejandromf78@users.noreply.github.com>
2025-02-12 12:19:18 +0100

    css quienesomos

--- DEFENRED/defenred1/defenred1/src/components/QuienesSomos.tsx
+++ DEFENRED/defenred1/defenred1/src/components/QuienesSomos.tsx
@@ -2,112 +2,116 @@ import "./css/quienesSomos.css";
 
 const QuienesSomos = () => {
   return (
-    
-    <div id="quienesSomos">
-      <h1>
-        ¿Quién es <strong>Defenred</strong>?
-      </h1>
-      <h2>
-        Comprometidos con el <strong>Apoyo</strong> y el{" "}
-        <strong>Bienestar</strong>de Defensores de{" "}
-        <strong>Derechos Humanos</strong>
-      </h2>
-      <p>
-        En <strong>Defenred</strong> creemos que un mundo más justo comienza con
-        los defensores de los <strong>derechos humanos</strong>. Sabemos que la
-        labor de un defensor de <strong>derechos humanos</strong> requiere un
-        compromiso constante, una dedicación incansable y muchas veces implica{" "}
-        <strong>riesgos</strong> que afectan tanto la{" "}
-        <strong>salud física</strong> como
-        <strong>mental</strong>. Pero, ¿qué sucede cuando quienes luchan por un{" "}
-        <strong>cambio positivo</strong>
-        necesitan <strong>apoyo</strong>? <strong>Defenred</strong> se fundó con
-        una misión clara: ofrecer a los
-        <strong>defensores</strong> de <strong>derechos humanos</strong> un
-        lugar de <strong>descanso</strong> y regeneración para que puedan
-        continuar con su valioso trabajo. Conscientes de las dificultades y
-        desafíos que enfrentan, entendemos que el <strong>autocuidado</strong>{" "}
-        no es un lujo sino una necesidad. Por eso en 2017 fundamos la{" "}
-        <strong>Casa de Respiro</strong> en <strong>España</strong>, un lugar
-        que tiene como objetivo brindar un descanso necesario a quienes se
-        encuentran en el frente de la <strong>defensa</strong> de los
-        <strong>derechos humanos</strong>. Aquí, los <strong>defensores</strong>{" "}
-        pueden encontrar tiempo para
-        <strong>descansar</strong>, recuperarse en un entorno pacífico y{" "}
-        <strong>seguro</strong>. Contamos con el
-        <strong>apoyo</strong> de expertos de la <strong>salud</strong> para
-        garantizar su bienestar y permitirles regresar a sus comunidades con{" "}
-        <strong>energía</strong> para continuar con su valiosa misión. En{" "}
-        <strong>Defenred</strong>, no sólo ofrecemos un lugar de relajación
-        física sino que también somos parte de una <strong>red global</strong>{" "}
-        de <strong>solidaridad</strong>. Al apoyar a los{" "}
-        <strong>defensores</strong> de <strong>derechos humanos</strong>,
-        fortalecemos a las <strong>comunidades</strong> que se preocupan por su
-        trabajo. Nuestro compromiso va más allá de brindar
-        <strong>espacio</strong>; creemos que al trabajar con quienes impulsan
-        el cambio, construimos un futuro más equitativo y digno para todos.
-        ¿Quiénes somos? Somos una mano amiga, brindando <strong>respiro</strong>
-        , paz y <strong>apoyo</strong> a quienes dedican su vida a la{" "}
-        <strong>justicia</strong>. Con cada <strong>defensor</strong> que
-        apoyamos, damos un paso hacia una sociedad más justa y humana.
-      </p>
-      <div id="TituloTestimonios">
-        <h2>
-          Testimonios: Voces de los <strong>Defensores</strong> de{" "}
+    <div id="quienesSomosWrapper">
+      <div id="quienesSomos">
+        <h1 id="tituloQuienesSomos">
+          ¿Quién es <strong>Defenred</strong>?
+        </h1>
+        <h2 id="subtituloQuienesSomos">
+          Comprometidos con el <strong>Apoyo</strong> y el{" "}
+          <strong>Bienestar </strong>de Defensores de{" "}
           <strong>Derechos Humanos</strong>
         </h2>
-        <h4>
-          Testimonios de nuestros <strong>defensores</strong>
-        </h4>
-      </div>
+        <p>
+          En <strong>Defenred</strong> creemos que un mundo más justo comienza
+          con los defensores de los <strong>derechos humanos</strong>. Sabemos
+          que la labor de un defensor de <strong>derechos humanos</strong>{" "}
+          requiere un compromiso constante, una dedicación incansable y muchas
+          veces implica <strong>riesgos</strong> que afectan tanto la{" "}
+          <strong>salud física</strong> como
+          <strong>mental</strong>. Pero, ¿qué sucede cuando quienes luchan por
+          un <strong>cambio positivo</strong>
+          necesitan <strong>apoyo</strong>? <strong>Defenred</strong> se fundó
+          con una misión clara: ofrecer a los
+          <strong>defensores</strong> de <strong>derechos humanos</strong> un
+          lugar de <strong>descanso</strong> y regeneración para que puedan
+          continuar con su valioso trabajo. Conscientes de las dificultades y
+          desafíos que enfrentan, entendemos que el <strong>autocuidado</strong>{" "}
+          no es un lujo sino una necesidad. Por eso en 2017 fundamos la{" "}
+          <strong>Casa de Respiro</strong> en <strong>España</strong>, un lugar
+          que tiene como objetivo brindar un descanso necesario a quienes se
+          encuentran en el frente de la <strong>defensa</strong> de los
+          <strong>derechos humanos</strong>. Aquí, los{" "}
+          <strong>defensores</strong> pueden encontrar tiempo para
+          <strong>descansar</strong>, recuperarse en un entorno pacífico y{" "}
+          <strong>seguro</strong>. Contamos con el
+          <strong>apoyo</strong> de expertos de la <strong>salud</strong> para
+          garantizar su bienestar y permitirles regresar a sus comunidades con{" "}
+          <strong>energía</strong> para continuar con su valiosa misión. En{" "}
+          <strong>Defenred</strong>, no sólo ofrecemos un lugar de relajación
+          física sino que también somos parte de una <strong>red global</strong>{" "}
+          de <strong>solidaridad</strong>. Al apoyar a los{" "}
+          <strong>defensores</strong> de <strong>derechos humanos</strong>,
+          fortalecemos a las <strong>comunidades</strong> que se preocupan por
+          su trabajo. Nuestro compromiso va más allá de brindar
+          <strong>espacio</strong>; creemos que al trabajar con quienes impulsan
+          el cambio, construimos un futuro más equitativo y digno para todos.
+          ¿Quiénes somos? Somos una mano amiga, brindando{" "}
+          <strong>respiro</strong>, paz y <strong>apoyo</strong> a quienes
+          dedican su vida a la <strong>justicia</strong>. Con cada{" "}
+          <strong>defensor</strong> que apoyamos, damos un paso hacia una
+          sociedad más justa y humana.
+        </p>
+        <div id="contenedorTestimonios">
+          <div id="tituloTestimonios">
+            <h2>
+              Testimonios: Voces de nuestros <strong>Defensores</strong> de{" "}
+              <strong>Derechos Humanos</strong>
+            </h2>
+          </div>
 
-      <div id="testimonios">
-        <div>
-          <h4>Adriana Diosa – Colombia</h4>
-          <p>
-            “Nos pusimos en contacto con <strong>Defenred</strong> por razones
-            de agotamiento.” Después de años de lucha contra violaciones de{" "}
-            <strong>derechos humanos</strong> en mi país, encontré en la{" "}
-            <strong>Casa de Respiro</strong> un <strong>espacio seguro</strong>{" "}
-            para
-            <strong>desconectar</strong> y retomar fuerzas. El ritmo de trabajo
-            y las experiencias tan duras pueden llevarnos al límite, y aquí, en
-            este retiro, agradezco profundamente el{" "}
-            <strong>apoyo y la tranquilidad</strong> que me brindaron en un
-            momento tan crítico para mí.
-          </p>
-        </div>
-        <div>
-          <h4>Sipho – Suazilandia</h4>
-          <p>
-            “Regreso a mi país renovado y listo para seguir adelante.” Regreso a
-            mi país renovado y listo para seguir adelante. Como{" "}
-            <strong>defensor</strong> de
-            <strong>derechos humanos</strong> en África, la{" "}
-            <strong>Casa de Respiro</strong> me brindó el{" "}
-            <strong>descanso</strong>y la paz que necesitaba para continuar mi
-            labor. Esta <strong>experiencia</strong> me permitió recargar
-            energías y pensar en nuevas estrategias para enfrentar los desafíos
-            que vienen. Me voy de la Sierra de <strong>Madrid</strong> con
-            fuerzas renovadas para seguir luchando por la{" "}
-            <strong>justicia</strong> y la <strong>igualdad</strong>
-            en mi <strong>comunidad</strong>.
-          </p>
-        </div>
-        <div>
-          <h4>Viktor Arias – Colombia</h4>
-          <p>
-            Para mí, la <strong>Casa de Respiro</strong> es un verdadero{" "}
-            <strong>espacio de acogida y descanso</strong>. Aquí, cada{" "}
-            <strong>defensor</strong> encuentra <strong>apoyo emocional</strong>{" "}
-            y físico en un ambiente de <strong>solidaridad</strong> y{" "}
-            <strong>comunidad</strong>. La <strong>desconexión</strong> que
-            ofrece la Casa es necesaria para retomar fuerzas y continuar con la
-            labor que hacemos. Valoro especialmente el contacto con otros{" "}
-            <strong>defensores</strong>, que nos permite compartir experiencias
-            y reforzar nuestro compromiso en la <strong>defensa</strong> de los{" "}
-            <strong>derechos humanos</strong>.
-          </p>
+          <div id="testimonios" className="grid grid33">
+            <div className="testimonio">
+              <h4>Adriana Diosa – Colombia</h4>
+              <p>
+                “Nos pusimos en contacto con <strong>Defenred</strong> por
+                razones de agotamiento.” Después de años de lucha contra
+                violaciones de <strong>derechos humanos</strong> en mi país,
+                encontré en la <strong>Casa de Respiro</strong> un{" "}
+                <strong>espacio seguro</strong> para
+                <strong>desconectar</strong> y retomar fuerzas. El ritmo de
+                trabajo y las experiencias tan duras pueden llevarnos al límite,
+                y aquí, en este retiro, agradezco profundamente el{" "}
+                <strong>apoyo y la tranquilidad</strong> que me brindaron en un
+                momento tan crítico para mí.
+              </p>
+            </div>
+            <div className="testimonio">
+              <h4>Sipho – Suazilandia</h4>
+              <p>
+                “Regreso a mi país renovado y listo para seguir adelante.”
+                Regreso a mi país renovado y listo para seguir adelante. Como{" "}
+                <strong>defensor</strong> de
+                <strong>derechos humanos</strong> en África, la{" "}
+                <strong>Casa de Respiro</strong> me brindó el{" "}
+                <strong>descanso</strong>y la paz que necesitaba para continuar
+                mi labor. Esta <strong>experiencia</strong> me permitió recargar
+                energías y pensar en nuevas estrategias para enfrentar los
+                desafíos que vienen. Me voy de la Sierra de{" "}
+                <strong>Madrid</strong> con fuerzas renovadas para seguir
+                luchando por la <strong>justicia</strong> y la{" "}
+                <strong>igualdad</strong>
+                en mi <strong>comunidad</strong>.
+              </p>
+            </div>
+            <div className="testimonio">
+              <h4>Viktor Arias – Colombia</h4>
+              <p>
+                Para mí, la <strong>Casa de Respiro</strong> es un verdadero{" "}
+                <strong>espacio de acogida y descanso</strong>. Aquí, cada{" "}
+                <strong>defensor</strong> encuentra{" "}
+                <strong>apoyo emocional</strong> y físico en un ambiente de{" "}
+                <strong>solidaridad</strong> y <strong>comunidad</strong>. La{" "}
+                <strong>desconexión</strong> que ofrece la Casa es necesaria
+                para retomar fuerzas y continuar con la labor que hacemos.
+                Valoro especialmente el contacto con otros{" "}
+                <strong>defensores</strong>, que nos permite compartir
+                experiencias y reforzar nuestro compromiso en la{" "}
+                <strong>defensa</strong> de los{" "}
+                <strong>derechos humanos</strong>.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
